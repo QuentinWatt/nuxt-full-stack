e78c6c9ebda009af87b82e811db7c5a8516ee8e7
Quentin Watt <quentin@qcasts.com>
2024-03-14 10:11:10 +0200

    Add search query for users

--- server/api/users/index.get.ts
+++ server/api/users/index.get.ts
@@ -1,8 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, type Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  const users = await prisma.user.findMany();
+  const { search } = getQuery(event);
+
+  let usersQuery: Prisma.UserFindManyArgs = {};
+
+  if (search) {
+    usersQuery = {
+      where: {
+        OR: [
+          {
+            name: {
+              contains: search,
+            },
+          },
+          {
+            email: {
+              contains: search,
+            },
+          },
+        ].filter(Boolean) as Prisma.UserWhereInput[],
+      },
+    };
+  }
+
+  const users = await prisma.user.findMany(usersQuery);
 
   return {
     data: users,
