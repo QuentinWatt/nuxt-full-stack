ee156c9b1dbc89a8bed7d35612ca416e6a69414c
Quentin Watt <quentin@qcasts.com>
2024-03-15 15:16:50 +0200

    Add github action for tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from "nuxt/config";
+import Vue from "@vitejs/plugin-vue";
 
 export default defineNuxtConfig({
+  plugins: [Vue()],
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
