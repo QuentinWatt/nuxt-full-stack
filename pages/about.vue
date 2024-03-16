8247df8c867a51e24160bc14dda8b88cd69500a5
Quentin Watt <quentin@qcasts.com>
2024-03-16 10:09:23 +0200

    add container to home page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="container mx-auto px-3 py-6">
     <h1>About us</h1>
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="container mx-auto px-3 py-6">
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
